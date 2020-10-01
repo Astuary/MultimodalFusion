b3977905009bad99e9b262f3ba778df2bce4aacb
Astuary <32832266+Astuary@users.noreply.github.com>
2020-10-01 09:03:10 -0400

    Created Dataloaders

---
+++ Data/constants/paths.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+# path to the SDK folder
+SDK_PATH: Optional[str] = "..\Dataset\CMU-MultimodalSDK"
+
+# path to the folder where you want to store data
+DATA_PATH_HIGH_LEVEL: Optional[str] = "..\Dataset\HighLevelData\high_level"
+DATA_PATH_LABELS: Optional[str] = "..\Dataset\HighLevelData\labels"
+ALIGNED_DATA_PATH_HIGH_LEVEL: Optional[str] = "..\Dataset\AlignedData\high_level"
+ALIGNED_DATA_PATH_LABELS: Optional[str] = "..\Dataset\AlignedData\labels"
+
+# path to a pretrained word embedding file
+WORD_EMB_PATH: Optional[str] = None
+
+# path to loaded word embedding matrix and corresponding word2id mapping
+CACHE_PATH: Optional[str] = '..\embedding_and_mapping.pt'
+
---
+++ Dataset/stub.md
@@ -0,0 +1,14 @@
+This is just a stub file to include the Dataset folder in the repository.
+
+The actual structure of this folder looks like this:
+Dataset
+- AlignedData
+  - high_level
+  - labels
+
+- HighLevelData
+  - high_level
+  - labels
+
+- CMU-MultimodalSDK
+  - [all the folders inside the SDK repo]
